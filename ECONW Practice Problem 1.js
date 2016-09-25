288b878cb0d3a0e04ca7d2848724e6ac1569d67b
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-09-25 12:06:48 -0400

    Added prompt

---
+++ ECONW Practice Problem 1.js
@@ -0,0 +1,5 @@
+/*Generate first five fibonachi numbers that are divisible by three
+
+Now, consider an ordered list of prime numbers, including and starting at 1.
+What is the sum of the prime numbers whose positions in the list of prime numbers
+are those five numbers from the Fibonacci sequence?*/
