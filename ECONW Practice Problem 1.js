160b47bae6dc6945683f00cf3ac51eacaf782a37
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-09-25 12:16:43 -0400

    Initial untested solution for first portion of problem

--- ECONW Practice Problem 1.js
+++ ECONW Practice Problem 1.js
@@ -1,5 +1,24 @@
-/*Generate first five fibonachi numbers that are divisible by three
+/*Generate first five Fibonacci numbers that are divisible by three
 
 Now, consider an ordered list of prime numbers, including and starting at 1.
 What is the sum of the prime numbers whose positions in the list of prime numbers
 are those five numbers from the Fibonacci sequence?*/
+
+//1. Generate a list of Fibonacci numbers; will include some kind of a loop involving initial values and indexing
+//2. Use conditionals to check if the current fibonachi number is divisible by 3
+//3. Add that number to an array
+
+function fibBy3(){
+  // index0 = 0;
+  // index1 = 1;
+  fibs = [0, 1];
+  answer = [];
+  while (answer.length < 5){
+    for (i in fibs){
+      fibs.push(fibs[:2] + fibs[:1]);
+      if (fibs[:1] % 3 === 0){
+        answer.push(fibs[:1]);
+      }
+    }
+  }
+}
