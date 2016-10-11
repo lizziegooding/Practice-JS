3969e747df9e3a4c0b5a5d5ddd7fce379abdb174
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-10-11 14:13:15 -0400

    Finished comments

--- Mapbox-Distance-API/script.js
+++ Mapbox-Distance-API/script.js
@@ -1,12 +1,12 @@
-// Sandbox for Mapbox-Distance-API
+// Sandbox for Mapbox Distance API
 
   // Personal access token
 mapboxgl.accessToken = 'pk.eyJ1IjoibGl6emllZ29vZGluZyIsImEiOiJjaW92cmc1NHYwMWJsdW9tOHowdTA2cnFsIn0.lFq-Wju99kZ_dR_2TMBYCQ';
 
 var euclidean;
 var network;
-var networkH1 = $('#output h1:nth-child(1)');
-var euclideanH1 = $('#output h1:nth-child(2)');
+var displayNetwork = $('#output h1:nth-child(1)');
+var displayEuclidean = $('#output h1:nth-child(2)');
 
 //Add a new Mapbox GL Map object
 var map = new mapboxgl.Map({
@@ -20,33 +20,17 @@ var map = new mapboxgl.Map({
   zoom: 10
 });
 
-// var distanceContainer = document.getElementById('distance');
-
-// GeoJSON object to hold our measurement features
-// var geojson = {
-//   'type': 'FeatureCollection',
-//   'features': []
-// };
-
-// Used to draw a line between points
-// var linestring = {
-//   'type': 'Feature',
-//   'geometry': {
-//     'type': 'LineString',
-//     'coordinates': []
-//   }
-// };
-
 //Add Driving Directions plugin
 map.addControl(new mapboxgl.Directions());
 
 var directions = new mapboxgl.Directions({
   unit: 'metric', // Use the metric system to display distances.
-  profile: 'driving', // Set the initial profile to walking.
+  profile: 'driving', // Set the initial profile to driving.
   container: 'directions', // Specify an element thats not the map container.
   proximity: [-122.6765, 45.5231] // Give search results closer to these coordinates higher priority.
 });
 
+//Declare euclidean line
 var eucLine = {
   'type': 'Feature',
   'geometry': {
@@ -56,13 +40,18 @@ var eucLine = {
 };
 
 map.on('load', function() {
-  directions.setOrigin('Portland, Oregon'); // On load, set the origin to 'Toronto, Ontario'.
-  directions.setDestination('Beaverton, Oregon'); // On load, set the destination to 'Montreal, Quebec'.
+  // On load, set the origin to 'Portland, Oregon'
+  directions.setOrigin('Portland, Oregon');
+  // Set the destination to 'Beaverton, Oregon'
+  directions.setDestination('Beaverton, Oregon');
+
+  //Add Euclidean line source
   map.addSource('eucLine',{
     'type': 'geojson',
     'data': eucLine
   });
 
+  //Add Euclidean line layer
   map.addLayer({
     'id': 'eucLine',
     'type': 'line',
@@ -79,22 +68,23 @@ map.on('load', function() {
 
 });
 
+//When the route event occurs
 directions.on('route', function(e) {
-  // Logs the current route shown in the interface.
-  console.log(e.route);
-
-  //Returns geojson object of origin point
+  //Return geojson object of origin point
   origin = directions.getOrigin();
-
-  //Returns geojson object of destination point
+  //Return geojson object of destination point
   destination = directions.getDestination();
 
+  //Set eucLine coordinates to route coordinates
   eucLine.geometry.coordinates = [origin.geometry.coordinates, destination.geometry.coordinates];
+  //Update map with new data
   map.getSource('eucLine').setData(eucLine);
 
-  //Write distance to DOM
+  //Write network distance to DOM
   network = e.route[0].distance;
-  networkH1.html('Network Distance: ' + (network / 1000) + ' km');
+  displayNetwork.text('Network Distance: ' + (network / 1000) + ' km');
+
+  //Write euclidean distance to DOM
   euclidean = turf.lineDistance(eucLine);
-  euclideanH1.html('Euclidean Distance: ' + euclidean + ' km');
+  displayEuclidean.text('Euclidean Distance: ' + euclidean + ' km');
 });
