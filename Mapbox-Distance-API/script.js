62702d8134bb5e886ffb709aeaa21abbd7a7bc22
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-10-11 11:34:47 -0400

    Commented existing code, wrote to document text to display map output

--- Mapbox-Distance-API/script.js
+++ Mapbox-Distance-API/script.js
@@ -1,14 +1,19 @@
 // Sandbox for Mapbox-Distance-API
-$(document).ready(function() {
-  mapboxgl.accessToken = 'pk.eyJ1IjoibGl6emllZ29vZGluZyIsImEiOiJjaW92cmc1NHYwMWJsdW9tOHowdTA2cnFsIn0.lFq-Wju99kZ_dR_2TMBYCQ';
 
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [-79.4512, 43.6568],
-    zoom: 13
-  });
+  // Personal access token
+mapboxgl.accessToken = 'pk.eyJ1IjoibGl6emllZ29vZGluZyIsImEiOiJjaW92cmc1NHYwMWJsdW9tOHowdTA2cnFsIn0.lFq-Wju99kZ_dR_2TMBYCQ';
 
-  map.addControl(new mapboxgl.Directions());
-
+//Add a new Mapbox GL Map object
+var map = new mapboxgl.Map({
+  // Place inside #map div
+  container: 'map',
+  // Use streets basemap
+  style: 'mapbox://styles/mapbox/streets-v9',
+  // Center on Portland, OR
+  center: [-122.6765, 45.5231],
+  //Zoom 10 to see full city
+  zoom: 10
 });
+
+//Add Driving Directions plugin
+map.addControl(new mapboxgl.Directions());
