57f7a916a718059a00e1e5e2d6ba87543ecd2554
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-10-11 11:27:52 -0400

    Added sample Mapbox code for Distance API plugin, Mapbox GL JS

---
+++ Mapbox-Distance-API/script.js
@@ -0,0 +1,14 @@
+// Sandbox for Mapbox-Distance-API
+$(document).ready(function() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibGl6emllZ29vZGluZyIsImEiOiJjaW92cmc1NHYwMWJsdW9tOHowdTA2cnFsIn0.lFq-Wju99kZ_dR_2TMBYCQ';
+
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-79.4512, 43.6568],
+    zoom: 13
+  });
+
+  map.addControl(new mapboxgl.Directions());
+
+});
